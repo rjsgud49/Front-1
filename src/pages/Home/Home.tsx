b862feee1bafa78911a804b36a4fe853d386884a
rlaxogh76 <btm.email2769@gmail.com>
2025-11-13 14:50:20 +0900

    refactor: Home 컴포넌트의 코드 스타일 개선

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,23 +1,23 @@
-import BlogCards from './BlogCards';
-import ChannelSection from './ChannelSection';
-import ContributorRanking from './ContributorRanking';
-import EventBanner from './EventBanner';
-import LatestPosts from './LatestPosts';
+import BlogCards from "./BlogCards";
+import ChannelSection from "./ChannelSection";
+import ContributorRanking from "./ContributorRanking";
+import EventBanner from "./EventBanner";
+import LatestPosts from "./LatestPosts";
 
 const Home = () => {
-    return (
-        <div className="flex flex-col lg:flex-row gap-6 px-4 lg:px-8 py-8 bg-gray-50">
-            <ContributorRanking />
+  return (
+    <div className="flex flex-col gap-6 px-4 py-8 lg:flex-row lg:px-8 bg-gray-50">
+      <ContributorRanking />
 
-            <main className="flex-1 flex flex-col gap-6">
-                <EventBanner />
-                <ChannelSection />
-                <BlogCards />
-            </main>
+      <main className="flex flex-col flex-1 gap-6">
+        <EventBanner />
+        <ChannelSection />
+        <BlogCards />
+      </main>
 
-            <LatestPosts />
-        </div>
-    );
+      <LatestPosts />
+    </div>
+  );
 };
 
 export default Home;
