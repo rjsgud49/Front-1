aa5d9b5db408a18566ae259a0f8ae8ef3834be22
rlaxogh76 <btm.email2769@gmail.com>
2025-11-17 18:01:02 +0900

    fix: vitest 설정 파일에서 불필요한 주석 제거

--- vite.config.ts
+++ vite.config.ts
@@ -5,31 +5,21 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
 import { playwright } from "@vitest/browser-playwright";
-
 const dirname =
   typeof __dirname !== "undefined"
     ? __dirname
     : path.dirname(fileURLToPath(import.meta.url));
 
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  base: "/lipsum/",
   plugins: [tailwindcss()],
-
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://3.38.107.119:8080", // EC2 API 서버
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-
   test: {
     projects: [
       {
         extends: true,
         plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
           storybookTest({
             configDir: path.join(dirname, ".storybook"),
           }),
