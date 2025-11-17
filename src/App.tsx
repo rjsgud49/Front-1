395c52aa594bba5e449fd3261877072d0e33c96d
rlaxogh76 <btm.email2769@gmail.com>
2025-11-17 11:00:46 +0900

    feat: CommunityList 컴포넌트에 CommunityWrite 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,48 @@
 // src/App.tsx
-import { Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Home from './pages/Home/Home';
-import Community from './pages/Community/Community';
-import Contact from './pages/Contact/Contact';
-import Exchange from './pages/Exchange/Exchange';
-import Events from './pages/Event/Event';
-import Profile from './pages/Profile/Profile';
-import Login from './pages/Auth/Login';
-import NotFound from './pages/NotFound';
-import Blog from './pages/Blog/Blog';
-import Form from './pages/Contact/Form';
-import CommunityDetail from './pages/Community/CommunityDetail';
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+import Community from "./pages/Community/Community";
+import Contact from "./pages/Contact/Contact";
+import Exchange from "./pages/Exchange/Exchange";
+import Events from "./pages/Event/Event";
+import Profile from "./pages/Profile/Profile";
+import Login from "./pages/Auth/Login";
+import NotFound from "./pages/NotFound";
+import Blog from "./pages/Blog/Blog";
+import Form from "./pages/Contact/Form";
+import CommunityDetail from "./pages/Community/CommunityDetail";
+import CommunityWrite from "./pages/Community/CommunityWrite";
 
 function App() {
-    return (
-        <>
-            <Header />
+  return (
+    <>
+      <Header />
 
-            <Routes>
-                {/* 헤더 페이지 라우트 */}
-                <Route path="/" element={<Home />} />
+      <Routes>
+        {/* 헤더 페이지 라우트 */}
+        <Route path="/" element={<Home />} />
 
-                <Route path="/community" element={<Community />} />
-                <Route path="/community/:uuid" element={<CommunityDetail />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/community/:uuid" element={<CommunityDetail />} />
+        <Route path="/community/write" element={<CommunityWrite />} />
 
-                <Route path="/exchange" element={<Exchange />} />
-                <Route path="/events" element={<Events />} />
-                <Route path="/blog" element={<Blog />} />
+        <Route path="/exchange" element={<Exchange />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/blog" element={<Blog />} />
 
-                <Route path="/contact/form" element={<Form />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/profile" element={<Profile />} />
+        <Route path="/contact/form" element={<Form />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/profile" element={<Profile />} />
 
-                {/* 인증 관련 라우트 */}
-                <Route path="/login" element={<Login />} />
+        {/* 인증 관련 라우트 */}
+        <Route path="/login" element={<Login />} />
 
-                {/* 맨 마지막에 배치할 것!! */}
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </>
-    );
+        {/* 맨 마지막에 배치할 것!! */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
