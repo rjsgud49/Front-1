29729bc4dd826aca8624907c90d1504f3cd149c0
rlaxogh76 <btm.email2769@gmail.com>
2025-11-17 18:01:41 +0900

    feat: API 기본 URL을 절대 경로로 변경

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "/api",
+  baseURL: "http://3.38.107.119:8080",
   headers: { "Content-Type": "application/json" },
   // 쿠키 세션 쓰는 경우만 true, 토큰이면 false 유지
   withCredentials: false,
